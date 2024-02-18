96f0c448d8c59293c44e631ba9d8dc7ad0ac7bf5
Skitus <artur.demenskiy@everget.co>
2024-02-18 14:33:06 +0200

    fix: init project

--- init-project-script.sh
+++ init-project-script.sh
@@ -9,28 +9,8 @@ cp src/config/configuration.example.ts src/config/configuration.ts
 # Установка зависимостей
 yarn
 
-# Запуск в режиме разработки
-yarn start:dev &
-
-# Сохранение PID процесса
-YARN_PID=$!
-
-# Ждем некоторое время, чтобы процесс успел стартовать
-sleep 5
-
-# Остановка процесса
-pkill -P $YARN_PID yarn
-
-# Удаление папки node_modules, dist и файла yarn.lock
-rm -rf node_modules
-rm -rf dist
-rm -f yarn.lock
-
-# Установка зависимостей
-yarn
-
-# Запуск миграции
+# Run DB migration don`t forget to set up right DB in src/config/configuration.ts file`
 yarn migration:run
 
 # Запуск в режиме разработки
-yarn start:dev
+yarn start:dev 
